9ec1771371636769dc21806e0237030c3d0cd680
Ouch Bros <obrostk@gmail.com>
2022-12-02 16:05:56 +0700

    upload project

---
+++ api/TicketingWebAPI/TicketingWebAPI/Controllers/TicketingController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketingWebAPI.Models;
+
+namespace TicketingWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketingController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TicketingController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                                select * from ticketphase1
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TicketAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(TicketPhase1 ticket)
+        {
+            string query = @"
+                                insert into ticketphase1 (Summary,Description,Status,CreatedDate) values (@Summary,@Description,@Status,@CreatedDate)
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TicketAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Summary", ticket.Summary);
+                    myCommand.Parameters.AddWithValue("@Description", ticket.Description);
+                    myCommand.Parameters.AddWithValue("@Status", 1);
+                    myCommand.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
+                    myReader = myCommand.ExecuteReader();
+                   
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Data Added");
+        }
+    }
+}
---
+++ api/TicketingWebAPI/TicketingWebAPI/Models/TicketPhase1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketingWebAPI.Models
+{
+    public class TicketPhase1
+    {
+        public int Id { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ResolveDate { get; set; }
+        public string ResolveBy { get; set; }
+
+    }
+}
